97692dc74bc3d7c30bc9b459645ee8f1b0850428
adarsh-n <adarshn82@gmail.com>
2025-07-03 00:54:08 +0530

    adding files

--- infrastructure_files/main.tf
+++ infrastructure_files/main.tf
@@ -1,18 +1,63 @@
-provider "aws" {
-  region = "us-east-1"
+resource "aws_ecs_cluster" "dotnet_cluster" {
+  name = "dotnet-ecs-cluster"
 }
 
-module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "sample-cluster"
-  subnets         = ["subnet-1", "subnet-2"]
-  vpc_id          = "vpc-sample"
-  enable_irsa     = true
-  node_groups = {
-    default = {
-      desired_capacity = 2
-      max_capacity     = 3
-      min_capacity     = 1
-    }
+resource "aws_lb" "dotnet_alb" {
+  name               = "dotnet-app-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = module.vpc.public_subnets
+}
+
+resource "aws_lb_target_group" "dotnet_tg" {
+  name     = "dotnet-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
+  target_type = "ip"
+}
+
+resource "aws_lb_listener" "dotnet_listener" {
+  load_balancer_arn = aws_lb.dotnet_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.dotnet_tg.arn
   }
 }
+
+resource "aws_ecs_task_definition" "dotnet_task" {
+  family                   = "dotnet-app"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "512"
+  memory                   = "1024"
+  execution_role_arn       = aws_iam_role.ecs_task_execution.arn
+  container_definitions    = file("task_definition.json")
+}
+
+resource "aws_ecs_service" "dotnet_service" {
+  name            = "dotnet-service"
+  cluster         = aws_ecs_cluster.dotnet_cluster.id
+  launch_type     = "FARGATE"
+  task_definition = aws_ecs_task_definition.dotnet_task.arn
+  desired_count   = 2
+
+  network_configuration {
+    subnets         = module.vpc.public_subnets
+    security_groups = [aws_security_group.ecs_sg.id]
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.dotnet_tg.arn
+    container_name   = "dotnet-app"
+    container_port   = 80
+  }
+
+  depends_on = [aws_lb_listener.dotnet_listener]
+}
+
---
+++ infrastructure_files/vpc.tf
@@ -0,0 +1,14 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.0"
+
+  name = "ecs-dotnet-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  enable_dns_hostnames = true
+}
+
